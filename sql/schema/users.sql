c71e77189c4789009a86d431a1af02722f9caaa6
2hyes <2hyes.com@gmail.com>
2025-06-14 23:02:46 +0900

    MODIFY users table

--- sql/schema/users.sql
+++ sql/schema/users.sql
@@ -1,20 +1,24 @@
--- 고객정보 날짜 / 이름 / ID / 고객번호(전화번호)
+-- 유저 정보
+-- 샵 매니저 / 디자이너 / 일반 고객
 
 CREATE TABLE "public"."users" (
     "seq" SERIAL PRIMARY KEY, 
     "id" VARCHAR(50) UNIQUE NOT NULL, 
+    "user_type" VARCHAR(10) NOT NULL,
     "name" VARCHAR(100) NOT NULL, 
     "password" VARCHAR NOT NULL, 
     "phone_number" VARCHAR(20) UNIQUE,
+    "is_active" BOOLEAN NOT NULL DEFAULT TRUE,
     "created_time" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-    "updated_time" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-    "memo" VARCHAR
-)
-COMMENT ON COLUMN "public"."users"."seq" IS 'auto_increment sequence';
-COMMENT ON COLUMN "public"."users"."id" IS 'user id';
-COMMENT ON COLUMN "public"."users"."name" IS 'customer name';
-COMMENT ON COLUMN "public"."users"."password" IS 'user password';
-COMMENT ON COLUMN "public"."users"."phone_number" IS 'customer phone number';
-COMMENT ON COLUMN "public"."users"."created_time" IS 'record creation time';
-COMMENT ON COLUMN "public"."users"."updated_time" IS 'record last update time';
-COMMENT ON COLUMN "public"."users"."memo" IS 'additional details';
+    "updated_time" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(0)
+);
+
+COMMENT ON COLUMN "public"."users"."seq" IS 'users 테이블의 고유 시퀀스';
+COMMENT ON COLUMN "public"."users"."id" IS '사용자 고유 ID';
+COMMENT ON COLUMN "public"."users"."user_type" IS 'shop, designer, customer';
+COMMENT ON COLUMN "public"."users"."name" IS '사용자 이름';
+COMMENT ON COLUMN "public"."users"."password" IS '사용자 비밀번호';
+COMMENT ON COLUMN "public"."users"."phone_number" IS '휴대폰 번호';
+COMMENT ON COLUMN "public"."users"."created_time" IS '생성 시간';
+COMMENT ON COLUMN "public"."users"."updated_time" IS '최종 수정 시간';
+COMMENT ON COLUMN "public"."users"."is_active" IS '사용자 활성화 여부';
