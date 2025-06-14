37b0258a3ba1571cdca5106bcb83207947ec1ba0
2hyes <2hyes.com@gmail.com>
2025-06-14 23:02:56 +0900

    ADD shops table

---
+++ sql/schema/shops.sql
@@ -0,0 +1,21 @@
+-- 샵 정보
+-- 샵 고유 ID / 샵 이름 / 샵 전화번호 / 사업자등록번호 / 생성일시 / 수정일시
+
+CREATE TABLE  "public"."shops" (
+    seq SERIAL PRIMARY KEY, 
+    id VARCHAR(50) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    number VARCHAR(20) NOT NULL,
+    biz_number VARCHAR(20) NOT NULL,
+    created_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    updated_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    FOREIGN KEY (id) REFERENCES users(id)
+);
+
+COMMENT ON COLUMN shops.seq IS 'shops테이블의 고유 시퀀스';
+COMMENT ON COLUMN shops.id IS '샵 고유 ID';
+COMMENT ON COLUMN shops.name IS '샵 이름';
+COMMENT ON COLUMN shops.number IS '샵 전화번호';
+COMMENT ON COLUMN shops.biz_number IS '사업자등록번호';
+COMMENT ON COLUMN shops.created_time IS '생성일시';
+COMMENT ON COLUMN shops.updated_time IS '수정일시';
